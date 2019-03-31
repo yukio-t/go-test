a1e6a91ddc0d4e0c017c095deb891ddbbf1e42d5
yukio-t <tsunemoto.yukio@gmail.com>
2019-03-31 20:55:35 +0900

    1st commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
+)
+
+//templは1つのテンプレートを表す
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+//ServeHTTPはHTTPリクエストを処理する
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "アプリケーションのアドレス")
+	flag.Parse() //フラグを解釈します
+	//Gomniauthのセットアップ
+	gomniauth.SetSecurityKey("chattest201901")
+	gomniauth.WithProviders(
+		facebook.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/facebook"),
+		github.New("クライアントID", "秘密の値", "http://localhost:8080/auth/callback/github"),
+		google.New("890765986605-d9kpv1pacqqefdfrcid9v13i4fl95ud1.apps.googleusercontent.com", "4G_TxAtV_JSNFs6YzHvnVT4B", "http://localhost:8080/auth/callback/google"),
+	)
+	r := newRoom()
+	http.Handle("/chat", MustAuth(&templateHandler{filename: "chat.html"}))
+	http.Handle("/login", &templateHandler{filename: "login.html"})
+	http.HandleFunc("/auth/", loginHander)
+	http.Handle("/room", r)
+	// チャットルームを開始します。
+	go r.run()
+	// webサーバーを起動します。
+	log.Println("Webサーバーを開始します。ポート：", *addr)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
